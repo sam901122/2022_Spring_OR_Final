4a2cfe5d278a8b843cb214bb53d63b957dff8f51
Leng-Kai <b07705049@ntu.edu.tw>
2022-06-05 15:28:36 +0800

    output results

--- YouBike/run_solver.py
+++ YouBike/run_solver.py
@@ -25,41 +25,83 @@ def rotate(locations, theta):
         for [x, y] in locations
     ]
 
+def match(c_all, c_loc, c_weight, t_loc, dist):
+    c_all_new = []
+    c_loc_new = []
+    c_weight_new = []
+    for i in range(len(c_loc)):
+        c = c_loc[i]
+        a = True
+        for t in t_loc:
+            if abs(c[0] - t[0]) + abs(c[1] - t[1]) <= dist:
+                a = False
+                print("match {} to {}".format(c_all[i][1], t))
+                break
+        if a:
+            c_all_new.append(c_all[i])
+            c_loc_new.append(c)
+            c_weight_new.append(c_weight[i])
+    return c_all_new, c_loc_new, c_weight_new
 
-def evaluate(locations, distance, facilities):
+
+def evaluate(c_all, t_all, distance, facilities, groups, uncovered):
     eps = 0.001
 
     def dist(x1, y1, x2, y2):
         return abs(x1 - x2) + abs(y1 - y2)
 
-    # for [x1, y1] in locations:
-    #     for [x2, y2] in facilities:
-    #         print(dist(x1, y1, x2, y2), end=" ")
-    #     print()
+    c_loc = [[float(row[2]), float(row[3])] for row in c_all]
+    t_loc = [[float(row[2]), float(row[3])] for row in t_all]
 
     all_dists = [
-        min([dist(x1, y1, x2, y2) for [x2, y2] in facilities]) for [x1, y1] in locations
+        min([dist(x1, y1, x2, y2) for [x2, y2] in facilities]) for [x1, y1] in c_loc
     ]
     max_dist = max(all_dists)
     avg_dist = sum(all_dists) / len(all_dists)
+    real_uncovered = len([d for d in all_dists if d > distance - eps])
+
+    r = []
+    for j in range(len(t_all)):
+        [tx, ty] = t_loc[j]
+        max_group_dist = 0
+        for k in groups[j]:
+            max_group_dist = max(max_group_dist, dist(tx, ty, facilities[k][0], facilities[k][1]))
+        r.append(max_group_dist)
+    
+    print("stations:")
+    for i in range(len(facilities)):
+        print("{}\t({},\t{})".format(i + 1, round(facilities[i][0], 5), round(facilities[i][1], 5)))
+
+    print("groups:")
+    for j in range(len(t_loc)):
+        print("group {} ({}):".format(j + 1, t_all[j][1]))
+        for k in groups[j]:
+            print("\tstation {}\t({},\t{})".format(k + 1, round(facilities[i][0], 5), round(facilities[i][1], 5)))
+
+    print("number of stations:", len(facilities))
+    print("average distance: {}".format(sum(r) / len(t_loc)))
+    print("uncovered: {}".format(uncovered))
+
     return max_dist, avg_dist, max_dist <= distance + eps
 
 
 if __name__ == "__main__":
-    c_path, t_path = "cpoints.csv", "tpoints.csv"
+    c_path, t_path = "OR_cpoints.csv", "OR_tpoints.csv"
 
-    _, c_loc, c_weight, dist = preprocess(c_path)
-    _, t_loc, t_weight, _ = preprocess(t_path)
+    c_all, c_loc, c_weight, dist = preprocess(c_path)
+    t_all, t_loc, t_weight, _ = preprocess(t_path)
+    c_all, c_loc, c_weight = match(c_all, c_loc, c_weight, t_loc, dist)
 
     c_loc = rotate(c_loc, pi / 4)
     t_loc = rotate(t_loc, pi / 4)
 
-    facilities = solve(c_loc, c_weight, t_loc, t_weight, dist / sqrt(2))
+    # facilities, groups, uncovered = solve(c_loc, c_weight, t_loc, t_weight, dist / sqrt(2))
+    facilities, groups, uncovered = solve(c_loc, c_weight, t_loc, t_weight, dist / sqrt(2))
     facilities = rotate(facilities, -pi / 4)
 
-    # print(facilities)
-    # evaluate(c_loc, t_loc, dist, facilities)
-    max_dist, avg_dist, fea = evaluate(rotate(c_loc, -pi / 4), dist, facilities)
+    evaluate(c_all, t_all, dist, facilities, groups, uncovered)
+    # evaluate(c_all, t_all, c_loc, t_loc, dist, facilities, groups, uncovered)
+    # max_dist, avg_dist, fea = evaluate(rotate(c_loc, -pi / 4), dist, facilities)
     # print(max_dist, avg_dist, fea)
     # result_df = result_df.append(
     #     {
--- YouBike/solver.py
+++ YouBike/solver.py
@@ -42,7 +42,7 @@ def solve(c_loc, c_weight, t_loc, t_weight, dist):
             #         (set[max2][0] + set[max4][0]) / 2,
             #         (set[max2][1] + set[max4][1]) / 2,
             #     ]
-            
+
             xs = [l[0] for l in set]
             ys = [l[1] for l in set]
             return [(min(xs) + max(xs)) / 2, (min(ys) + max(ys)) / 2]
@@ -57,40 +57,42 @@ def solve(c_loc, c_weight, t_loc, t_weight, dist):
                 s = [
                     k
                     for k in range(len(loc))
-                    if x <= loc[k][0] <= x + dist*2 and y <= loc[k][1] <= y + dist*2
+                    if x <= loc[k][0] <= x + dist * 2 and y <= loc[k][1] <= y + dist * 2
                 ]
                 if len(s) == 0 or sorted(s) in sets:
                     continue
                 sets.append(sorted(s))
                 centers.append([x + dist, y + dist])
-                
+
         sets = sorted(sets)
         centers = [get_center([loc[k] for k in s], dist) for s in sets]
 
         return sets, centers
 
-
     import gurobipy as gp
 
     c_sum = sum(c_weight)
     t_sum = sum(t_weight)
-    c_weight = [w / c_sum for w in c_weight]
-    t_weight = [w / t_sum for w in t_weight]
-    ppc = c_sum / len(c_weight) / len(t_weight)
+    c_weight = [100 * w / c_sum for w in c_weight]
+    t_weight = [100 * w / t_sum for w in t_weight]
+    ppc = 100 / len(c_weight) / len(t_weight)
 
     sets, centers = find_sets(c_loc, dist)
     # print(sorted(sets))
 
     model = gp.Model()
 
-    B = range(len(c_loc)) # i
-    T = range(len(t_loc)) # j
+    B = range(len(c_loc))  # i
+    T = range(len(t_loc))  # j
     S = range(len(sets))  # k
-    
-    M = 1e9
+
+    M = 100000000
 
     s = [[1 if i in set else 0 for i in B] for set in sets]
-    d = [[distance(centers[k][0], centers[k][1], t_loc[j][0], t_loc[j][1]) for j in T] for k in S]
+    d = [
+        [distance(centers[k][0], centers[k][1], t_loc[j][0], t_loc[j][1]) for j in T]
+        for k in S
+    ]
 
     c = [model.addVar(0.0, 1.0, 0.0, gp.GRB.BINARY) for k in S]
     cov = [model.addVar(0.0, 1.0, 0.0, gp.GRB.BINARY) for i in B]
@@ -123,38 +125,67 @@ def solve(c_loc, c_weight, t_loc, t_weight, dist):
 
     for j in T:
         model.addConstr(sum([z[k][j] for k in S]) >= t_weight[j] - u * ppc - eps)
+        model.addConstr(sum([z[k][j] for k in S]) >= eps)
+
+    for k in S:
+        model.addConstr(sum([z[k][j] for j in T]) <= sum([g[i][k] for i in B]) + 10 * eps)
 
     r = [model.addVar(0.0, 10000.0, 0.0, gp.GRB.CONTINUOUS) for j in T]
 
     for k in S:
         for j in T:
-            # r[j] <= d[k][j] if b[k][j] == 1
+            # r[j] >= d[k][j] if b[k][j] == 1
             model.addConstr(d[k][j] - r[j] <= M * (1 - b[k][j]))
 
     m = model.addVar(0.0, 10000.0, 0.0, gp.GRB.CONTINUOUS)
     for j in T:
         model.addConstr(m >= r[j])
 
+    # model.addConstr(u == 0)
+
     w1 = 1
     w2 = 1
     w3 = 1
 
-    model.setObjective(w1 * (sum(c) / len(c_loc)) + w2 * (m / (dist * 10000)) + w3 * (u / len(c_loc)), gp.GRB.MINIMIZE)
-    # model.setObjective(sum(c), gp.GRB.MINIMIZE)
+    model.setObjective(
+        w1 * (sum(c) / len(c_loc)) + w2 * (sum(r) / (len(t_loc) * dist * 100)) + w3 * (u / len(c_loc)),
+        gp.GRB.MINIMIZE,
+    )
+    # model.setObjective(w1 * (sum(c) / len(c_loc)) + w3 * (u / len(c_loc)))
     model.optimize()
+
+    # for j in T:
+    #     print(t_weight[j] - u.X * ppc)
 
     facilities = []
     for i in S:
         if c[i].X:
             facilities.append(centers[i])
-            # print(sets[i])
-            # print(a[j].X, b[j].X)
-            # print(centers[i])
+    # sum_r = 0
     # for j in T:
-    #     print(r[j].X)
+    #     sum_r += r[j].X
+    print(len(facilities), "/", len(c_loc))
+    # print(sum_r / len(t_loc) / dist)
+    print(u.X, "/", len(c_loc))
+
+    ii = 0
+    kk = []
+    for k in S:
+        kk.append(ii)
+        if c[k].X:
+            ii += 1
+
+    groups = []
+    for j in T:
+        group = []
+        for k in S:
+            if b[k][j].X:
+                group.append(kk[k])
+        groups.append(group)
+
+    # uncovered = []
     # for i in B:
-    #     print(cov[i].X)
-    print(len(facilities), '/', len(c_loc))
-    print(m.X / dist)
-    print(u.X, '/', len(c_loc))
-    return facilities
+    #     if u[i].X:
+    #         uncovered.append(i)
+
+    return facilities, groups, u.X
